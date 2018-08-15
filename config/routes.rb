5356f98dcc94a1aebc9c3288aaa9f2cb8307d0c1
gisele beddouk <gisele.beddouk@gmail.com>
2018-08-15 13:30:37 +0200

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'about', to: 'pages#about', as: :about
+  # get 'contact', to: 'pages#contact', as: :contact
+
+  get "/tasks", to: "tasks#index"
+
 end
